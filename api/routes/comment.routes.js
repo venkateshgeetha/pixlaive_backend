20806f36a6978e8b23debc3f4dd995398ffb455a
venkateshgeetha <venkatvenkatesh23075@gmail.com>
2021-05-13 18:23:49 +0530

    update_comment

--- api/routes/comment.routes.js
+++ api/routes/comment.routes.js
@@ -8,3 +8,7 @@ const {
 const { add_comment } = require("../controllers/Comment");
 
 router.post("/comment", userValidationRules("comment"), validate, add_comment);
+
+router.get("/getpost_comments", getPost_comments);
+
+router.get("/delete_comment", delete_comment);
